78d0762e8bb85d77d3addf4bc3515f759fe08604
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-07-04 06:27:27 +0300

    started changing

--- src/utils/template.js
+++ src/utils/template.js
@@ -1,12 +1,12 @@
 const temp = {
-  "language": "ru",
-  "director": "Cloud Hero",
-  "duration": 23,
-  "year": "2020",
-  "description": "Разбираемся с возможностями Яндекс.Облака. Начнём с сервиса Object Storage. Рассмотрим три основных инструмента работы с Object Storage: графическую консоль, CLI, HTTP API (SDK boto3).",
-  "image": "https://i.ytimg.com/vi/n2OABTalei8/mqdefault.jpg",
-  "trailerLink": "https://youtu.be/n2OABTalei8",
-  "name": "Азы работы с Яндекс Object Storage (консоль, CLI, SDK)"
+  "language": "en",
+  "director": "Raul Terhes",
+  "duration": 7,
+  "year": "2021",
+  "description": "Learn how you can simplify the boilerplate code of Redux to that maximum by using createSlice() to create \"slices\" of state, which combines the createAction() and createReducer() into one.  Part of the Redux Toolkit Introduction series.",
+  "image": "https://i.ytimg.com/vi/oeLcLVQtS0M/mqdefault.jpg",
+  "trailerLink": "https://youtu.be/oeLcLVQtS0M",
+  "name": "Slices | createSlice | Redux Toolkit Introduction | React"
 }
 
 export default temp;
